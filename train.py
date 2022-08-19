3348fe7e86a6fe4d72dda98b483cbf4927f328f5
Hyunju Yang <iam@2tle.io>
2022-08-19 17:33:41 +0900

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,10 @@ from srgan_v2 import SRGAN
 EPOCHS = 300
 BATCH_SIZE = 1
 INTERVAL = 50
+DATASETS = ''
+LOW_WIDTH = 100
+LOW_HEIGHT = 100
 
-gan = SRGAN()
+gan = SRGAN(DATASETS, low_width = LOW_WIDTH, low_height = LOW_HEIGHT)
 gan.train(epochs=EPOCHS, batch_size=BATCH_SIZE, sample_interval=INTERVAL)
 gan.generator.save('models/SRGAN_EP%s' % (str(EPOCHS)))
