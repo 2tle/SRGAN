1d3cd53594fc93bf1af91c23c192de559233bc6e
Hyunju Yang <iam@2tle.io>
2022-08-19 15:38:09 +0900

    Initial commit

---
+++ train.py
@@ -0,0 +1,9 @@
+from srgan_v2 import SRGAN
+
+EPOCHS = 1000
+BATCH_SIZE = 1
+INTERVAL = 50
+
+gan = SRGAN()
+gan.train(epochs=EPOCHS, batch_size=BATCH_SIZE, sample_interval=INTERVAL)
+gan.generator.save('models/SRGAN_EP%s' % (str(EPOCHS)))
